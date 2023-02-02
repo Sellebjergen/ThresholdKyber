2c52cbf0b3e9bcde47960531f7ddc31314c1f286
kasperdi <kasperdi1412@gmail.com>
2023-02-02 09:40:40 +0100

    Added util.go

---
+++ TKyber/util.go
@@ -0,0 +1,26 @@
+package TKyber
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func mod(x, m int32) int32 {
+	res := x % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+func Reverse(input []int32) []int32 {
+	var output []int32
+
+	for i := len(input) - 1; i >= 0; i-- {
+		output = append(output, input[i])
+	}
+
+	return output
+}
