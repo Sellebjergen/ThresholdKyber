69d8295c66f6b9a393e8c54e97e46010448c174f
kasperdi <kasperdi1412@gmail.com>
2023-02-07 12:07:15 +0100

    Minor changes

--- indcpa_TKyber/indcpa_TKyber.go
+++ indcpa_TKyber/indcpa_TKyber.go
@@ -9,6 +9,18 @@ func Setup(params *owcpa.OwcpaParams, n int, t int) (*kyber.IndcpaPublicKey, []k
 	return owcpa.Setup(params, n, t)
 }
 
+func Enc(params *owcpa.OwcpaParams, msg []byte, pk *kyber.IndcpaPublicKey, delta int) {
+	var mp kyber.Poly
+	mp.ToMsg(msg)
+	x := make([]*kyber.Poly, delta)
+	for i := 0; i < delta; i++ {
+		x[i] = owcpa.SampleUnifPolynomial(2)
+	}
+	c := make([]*kyber.Poly, delta+1)
+	c[0].Add(&mp, F(x))
+
+}
+
 func PartDec() {
 
 }
@@ -17,6 +29,10 @@ func Combine() {
 
 }
 
-func Enc() {
+func F(x []*kyber.Poly) *kyber.Poly {
+	return nil
+}
 
+func G(x []*kyber.Poly) []byte {
+	return nil
 }
