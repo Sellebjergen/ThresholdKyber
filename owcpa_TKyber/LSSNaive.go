8635eb5669e051a0655d9cb8df63d9e8a4ac49ca
kasperdi <kasperdi1412@gmail.com>
2023-02-15 20:00:37 +0100

    Added LSSNaive.go

---
+++ owcpa_TKyber/LSSNaive.go
@@ -0,0 +1,15 @@
+package owcpa_TKyber
+
+import (
+	kyberk2so "ThresholdKyber.com/m/kyber-k2so"
+)
+
+type LSSNaive struct{}
+
+func (s *LSSNaive) Share(sk kyberk2so.PolyVec, n int, t int) [][]kyberk2so.PolyVec {
+	return ShareRepNaive(sk, n, t, true)
+}
+
+func (s *LSSNaive) Rec(d_is [][]kyberk2so.Poly, n int, t int) kyberk2so.Poly {
+	return RecRepNaive(d_is, n, t, true)
+}
