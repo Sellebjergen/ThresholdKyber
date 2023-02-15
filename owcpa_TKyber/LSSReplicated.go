0e4a54b0096d75c0c9129e6604f58dc821983396
kasperdi di <kasperdi1412@gmail.com>
2023-02-15 14:47:06 +0100

    Finished implementing Replicated Secret Sharing. Needs more testing.

--- owcpa_TKyber/LSSReplicated.go
+++ owcpa_TKyber/LSSReplicated.go
@@ -1,79 +1,72 @@
 package owcpa_TKyber
 
 import (
+	"fmt"
+
 	kyberk2so "ThresholdKyber.com/m/kyber-k2so"
+	"ThresholdKyber.com/m/util"
 )
 
-func RepShare(sk kyberk2so.PolyVec, n int) [][]kyberk2so.PolyVec {
-	skShares := make([]kyberk2so.PolyVec, len(sk))
+func RepShare(sk kyberk2so.PolyVec, n int, t int) [][]kyberk2so.PolyVec {
+	r := len(sk)
+	skShares := make([][]kyberk2so.Poly, r)
 
-	for i, e := range sk {
-		skShares[i] = SharePolynomial(e, n)
-	}
+	combinations := util.MakeCombinations(n, t)
+
+	fmt.Println(combinations)
 
 	// the total amount of shares
 	shares := make([][]kyberk2so.PolyVec, n)
+	for i := 0; i < n; i++ {
+		shares[i] = make([]kyberk2so.PolyVec, len(combinations))
+		for j := 0; j < len(combinations); j++ {
+			shares[i][j] = kyberk2so.PolyvecNew(r)
+		}
+	}
 
-	// player 1 should have
-	shares10 := kyberk2so.PolyVec{
-		kyberk2so.Poly{},
-		skShares[0][1],
-		skShares[0][2],
+	for i, e := range sk {
+		skShares[i] = SharePolynomial(e, len(combinations))
 	}
-	shares11 := kyberk2so.PolyVec{
-		kyberk2so.Poly{},
-		skShares[1][1],
-		skShares[1][2],
-	}
-	shares1 := []kyberk2so.PolyVec{
-		shares10,
-		shares11,
-	}
-	shares[0] = shares1
 
-	// player 2 should have
-	shares20 := kyberk2so.PolyVec{
-		skShares[0][0],
-		kyberk2so.Poly{},
-		skShares[0][2],
-	}
-	shares21 := kyberk2so.PolyVec{
-		skShares[1][0],
-		kyberk2so.Poly{},
-		skShares[1][2],
-	}
-	shares2 := []kyberk2so.PolyVec{
-		shares20,
-		shares21,
-	}
-	shares[1] = shares2
+	// Players
+	for i := 0; i < n; i++ {
+		// Combinations
+		for j := 0; j < len(combinations); j++ {
+			comb := combinations[j]
+			if !util.Contains(comb, i+1) {
+				// Iterate over the r = k polynomials of the sk
+				for poly_num := 0; poly_num < r; poly_num++ {
+					copy(shares[i][j][poly_num][:], skShares[poly_num][j][:])
+				}
 
-	// player 3 should have
-	shares30 := kyberk2so.PolyVec{
-		skShares[0][0],
-		skShares[0][1],
-		kyberk2so.Poly{},
-	}
-	shares31 := kyberk2so.PolyVec{
-		skShares[1][0],
-		skShares[1][1],
-		kyberk2so.Poly{},
-	}
-	shares3 := []kyberk2so.PolyVec{
-		shares30,
-		shares31,
-	}
-	shares[2] = shares3
+			}
 
-	// combines the different shares
-	combinedShares := make([][]kyberk2so.PolyVec, n)
-	combinedShares[0] = shares1
-	combinedShares[1] = shares2
-	combinedShares[2] = shares3
+		}
 
-	return combinedShares
+	}
+
+	return shares
 }
 
-func RepRec(d_is []kyberk2so.Poly) kyberk2so.Poly {
-	return *new(kyberk2so.Poly)
+func RepRec(d_is [][]kyberk2so.Poly, n int, t int) kyberk2so.Poly {
+	var p1 kyberk2so.Poly
+
+	combinations := util.MakeCombinations(n, t)
+
+	for j := 0; j < len(combinations); j++ {
+		isFound := false
+		comb := combinations[j]
+		for i := 0; i < n; i++ {
+			if !util.Contains(comb, i+1) {
+				p1 = kyberk2so.PolyAdd(p1, d_is[i][j])
+				isFound = true
+			}
+		}
+
+		if isFound {
+			continue
+		}
+	}
+
+	return p1
 }
--- owcpa_TKyber/LSSReplicated_test.go
+++ owcpa_TKyber/LSSReplicated_test.go
@@ -1,16 +1,17 @@
 package owcpa_TKyber
 
 import (
-	kyberk2so "ThresholdKyber.com/m/kyber-k2so"
 	"reflect"
 	"testing"
+
+	kyberk2so "ThresholdKyber.com/m/kyber-k2so"
 )
 
 func TestShareGivesCorrectAmountOfShares(t *testing.T) {
 	amountOfPlayers := 3
 	MockKyber512Key := SampleUniformPolyVec(17, 2)
 
-	shares := RepShare(MockKyber512Key, amountOfPlayers)
+	shares := RepShare(MockKyber512Key, amountOfPlayers, 1)
 
 	if !reflect.DeepEqual(len(shares), amountOfPlayers) {
 		t.Errorf("Something went wrong, combination of shares are not equal starting value!")
@@ -21,17 +22,17 @@ func TestSharesCanBeReconstructed(t *testing.T) {
 	amountOfPlayers := 3
 	MockKyber512Key := SampleUniformPolyVec(17, 2)
 
-	shares := RepShare(MockKyber512Key, amountOfPlayers)
+	shares := RepShare(MockKyber512Key, amountOfPlayers, 1)
 
 	var p1 kyberk2so.Poly
-	p1 = kyberk2so.PolyAdd(p1, shares[0][0][1]) // player 1 share of first poly, Share 2
-	p1 = kyberk2so.PolyAdd(p1, shares[0][0][2]) // player 1 share of first poly, Share 3
+	p1 = kyberk2so.PolyAdd(p1, shares[0][1][0]) // player 1 share of first poly, Share 2
+	p1 = kyberk2so.PolyAdd(p1, shares[0][2][0]) // player 1 share of first poly, Share 3
 	p1 = kyberk2so.PolyAdd(p1, shares[1][0][0]) // player 2 share of first poly, Share 1
 
 	var p2 kyberk2so.Poly
 	p2 = kyberk2so.PolyAdd(p2, shares[0][1][1]) // player 1 share of second poly. Share 2
-	p2 = kyberk2so.PolyAdd(p2, shares[0][1][2]) // player 1 share of second poly. Share 3
-	p2 = kyberk2so.PolyAdd(p2, shares[2][1][0]) // player 3 share of second poly. Share 1
+	p2 = kyberk2so.PolyAdd(p2, shares[0][2][1]) // player 1 share of second poly. Share 3
+	p2 = kyberk2so.PolyAdd(p2, shares[2][0][1]) // player 3 share of second poly. Share 1
 
 	reconstructed := kyberk2so.PolyVec{p1, p2}
 
@@ -45,23 +46,54 @@ func TestSharesCanBeReconstructedUsingARealKyberKey(t *testing.T) {
 	sk, _, _ := kyberk2so.IndcpaKeypair(2)
 	unpackedSk := kyberk2so.IndcpaUnpackPrivateKey(sk, 2)
 
-	shares := RepShare(unpackedSk, amountOfPlayers)
+	shares := RepShare(unpackedSk, amountOfPlayers, 1)
 
 	// todo: this seems like the nesting is to deep? shouldn't this only be a matrix?
 	var p1 kyberk2so.Poly
 	// take 2 shares from player 1 and the last share he does not have from player 2
-	p1 = kyberk2so.PolyAdd(p1, shares[0][0][1]) // player 1 share of first poly, Share 2
-	p1 = kyberk2so.PolyAdd(p1, shares[0][0][2]) // player 1 share of first poly, Share 3
+	p1 = kyberk2so.PolyAdd(p1, shares[0][1][0]) // player 1 share of first poly, Share 2
+	p1 = kyberk2so.PolyAdd(p1, shares[0][2][0]) // player 1 share of first poly, Share 3
 	p1 = kyberk2so.PolyAdd(p1, shares[1][0][0]) // player 2 share of first poly, Share 1
 
 	var p2 kyberk2so.Poly
 	p2 = kyberk2so.PolyAdd(p2, shares[0][1][1]) // player 1 share of second poly. Share 2
-	p2 = kyberk2so.PolyAdd(p2, shares[0][1][2]) // player 1 share of second poly. Share 3
-	p2 = kyberk2so.PolyAdd(p2, shares[2][1][0]) // player 3 share of second poly. Share 1
+	p2 = kyberk2so.PolyAdd(p2, shares[0][2][1]) // player 1 share of second poly. Share 3
+	p2 = kyberk2so.PolyAdd(p2, shares[2][0][1]) // player 3 share of second poly. Share 1
 
 	reconstructed := kyberk2so.PolyVec{p1, p2}
 
 	if !reflect.DeepEqual(unpackedSk, reconstructed) {
 		t.Errorf("Something went wrong assembling the mock key!")
+	}
+}
+
+func TestSinglePolyCanBeReconstructed(t *testing.T) {
+	toShare := []kyberk2so.Poly{{1, 2, 3, 4, 5, 6}}
+	shared := RepShare(toShare, 3, 1)
+
+	var p1 kyberk2so.Poly
+	p1 = kyberk2so.PolyAdd(p1, shared[0][1][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[0][2][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[1][0][0])
+
+	if !reflect.DeepEqual(toShare[0], p1) {
+		t.Errorf("WEE WOO WEE WOO")
+	}
+}
+
+func TestSinglePolyCanBeReconstructedN4T2(t *testing.T) {
+	toShare := []kyberk2so.Poly{{1, 2, 3, 4, 5, 6}}
+	shared := RepShare(toShare, 4, 2)
+
+	var p1 kyberk2so.Poly
+	p1 = kyberk2so.PolyAdd(p1, shared[0][3][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[0][4][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[0][5][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[2][0][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[1][1][0])
+	p1 = kyberk2so.PolyAdd(p1, shared[1][2][0])
+
+	if !reflect.DeepEqual(toShare[0], p1) {
+		t.Errorf("WEE WOO WEE WOO")
 	}
 }
