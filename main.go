751fb0ab3ab80e40ba895b82591466fd57455290
kasperdi <kasperdi1412@gmail.com>
2023-05-21 10:18:40 +0200

    Removed old import in main.go

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"ThresholdKyber.com/m/ddec"
-	kyberk2so "ThresholdKyber.com/m/kyber-k2so"
-)
-
 func main() {
-	//ddec.Generate_test_vec(2)
-	ddec.Generate_Enc_Vec(kyberk2so.ParamsK)
 	//gladius.WriteDDecTestVec()
 }
